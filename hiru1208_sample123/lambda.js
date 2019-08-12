d1f408f971b9ba106d673dc83094d8efa70e8a3b
HirudineeADRT <hirudinee@adroitlogic.com>
2019-08-12 12:39:48 +0530

    changed global

--- hiru1208_sample123/lambda.js
+++ hiru1208_sample123/lambda.js
@@ -2,16 +2,18 @@ let AWS = require('aws-sdk');
 const cognito_idp = new AWS.CognitoIdentityServiceProvider();
 
 exports.handler = function (event, context, callback) {
+    debugger;
 
     cognito_idp.listUsers({
         UserPoolId: process.env.UserPoolId_cognitohirutest,
+        AttributesToGet: ["sub", "address", "birthdate", "email", "family_name", "given_name", "locale", "name", "nickname", "picture", "preferred_username", "profile", "timezone", "updated_at", "website"],
         Limit: 10
     }, function (error, data) {
         if (error) {
             // implement error handling logic here
             throw error;
         }
-        // your logic goes within this block
+        console.log(data)
     });
 
 
